7999736008dff0410a5ee21ac5548b07b9559b30
Ben MacAndrews <ben.macandrews@gmail.com>
2024-03-06 09:17:50 +0000

    adds color picker component

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { SketchPicker } from "react-color";
+import { useSnapshot } from 'valtio';
+
+import state from '../store'
 
 function ColorPicker() {
+  const snap = useSnapshot(state);
+
   return (
-    <div>ColorPicker</div>
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        presetColors={[
+          "#FF6F61", // Coral
+          "#FFD700", // Gold
+          "#FF69B4", // Hot Pink
+          "#00CED1", // Turquoise
+          "#FFA07A", // Light Salmon
+          "#32CD32", // Lime Green
+          "#BA55D3", // Medium Orchid
+          "#FF6347", // Tomato
+          "#FFD700", // Goldenrod
+          "#00FF7F", // Spring Green
+          "#9370DB", // Medium Purple
+          "#FF4500", // Orange Red
+          "#20B2AA", // Light Sea Green
+          "#FF1493", // Deep Pink
+          "#8A2BE2",  // Blue Violet
+          "#dde048", // Teya Lime
+          "#151515", // Teya Charcoal
+          "f5f5f5", // Teya Off white
+        ]}
+        onChange={(color) => state.color = color.hex}
+      />
+    </div>
   )
 }
 
