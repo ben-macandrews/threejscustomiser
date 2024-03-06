007c22a3f1d5315c2d19d5fd220a501bc5c701c0
Ben MacAndrews <ben.macandrews@gmail.com>
2024-03-06 15:56:56 +0000

    fixes filter tabs

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -66,6 +66,15 @@ const Customizer = () => {
         state.isLogoTexture = true;
         state.isFullTexture = false;
     }
+
+    // after setting the state, activeFilterTab is updated
+
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName]
+      }
+    })
   }
 
   const readFile = (type) => {
@@ -122,8 +131,8 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick={() => { }}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
           </motion.div>
